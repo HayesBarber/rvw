a03708ec1dfacf74020c92e3f56291b262d8f4f2
Hayes Barber <barberhayes@gmail.com>
2026-08-25 08:40:47 -0400

    obtain lock file for unix socket

--- src/dispatcher/unix_socket.zig
+++ src/dispatcher/unix_socket.zig
@@ -4,6 +4,7 @@ const json_protocol = @import("../app/json_protocol.zig");
 
 const Allocator = std.mem.Allocator;
 const frame_header_size = @sizeOf(u32);
+const lock_file_suffix = ".lock";
 
 pub const max_request_size: u32 = 1024 * 1024;
 
@@ -13,8 +14,21 @@ pub fn serve(
     dispatcher: dispatcher_module.Dispatcher,
     address: std.Io.net.UnixAddress,
 ) !void {
+    if (address.isAbstract()) return error.AbstractSocketUnsupported;
+
+    const lock_path = try std.mem.concat(allocator, u8, &.{ address.path, lock_file_suffix });
+    defer allocator.free(lock_path);
+    var lock_file = try acquireLock(io, .cwd(), lock_path);
+    defer lock_file.close(io);
+
+    try removeStaleSocket(io, .cwd(), address.path);
     var listener = try address.listen(io, .{});
-    defer listener.deinit(io);
+    defer {
+        listener.deinit(io);
+        removeStaleSocket(io, .cwd(), address.path) catch |err| {
+            std.log.warn("failed to remove Unix socket {s}: {t}", .{ address.path, err });
+        };
+    }
     var connections: std.Io.Group = .init;
     defer connections.cancel(io);
 
@@ -23,6 +37,25 @@ pub fn serve(
         const stream = try listener.accept(io);
         connections.async(io, serveConnection, .{ allocator, io, dispatcher, stream });
     }
+}
+
+fn acquireLock(io: std.Io, dir: std.Io.Dir, path: []const u8) !std.Io.File {
+    return dir.createFile(io, path, .{
+        .truncate = false,
+        .lock = .exclusive,
+        .lock_nonblocking = true,
+    }) catch |err| switch (err) {
+        error.WouldBlock => error.AlreadyRunning,
+        else => err,
+    };
+}
+
+fn removeStaleSocket(io: std.Io, dir: std.Io.Dir, path: []const u8) !void {
+    const stat = dir.statFile(io, path, .{ .follow_symlinks = false }) catch |err| switch (err) {
+        error.FileNotFound => return,
+        else => return err,
+    };
+    if (stat.kind == .unix_domain_socket) try dir.deleteFile(io, path);
 }
 
 fn serveConnection(
@@ -61,4 +94,61 @@ fn dispatchFrame(
     try writer.writeInt(u32, @intCast(response.len), .big);
     try writer.writeAll(response);
     try writer.flush();
+}
+
+test "lock file prevents a second dispatcher and is released on close" {
+    const io = std.testing.io;
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    {
+        var first = try acquireLock(io, tmp.dir, "rvw.sock.lock");
+        defer first.close(io);
+        try std.testing.expectError(
+            error.AlreadyRunning,
+            acquireLock(io, tmp.dir, "rvw.sock.lock"),
+        );
+    }
+
+    var reacquired = try acquireLock(io, tmp.dir, "rvw.sock.lock");
+    reacquired.close(io);
+    const stat = try tmp.dir.statFile(io, "rvw.sock.lock", .{});
+    try std.testing.expectEqual(std.Io.File.Kind.file, stat.kind);
+}
+
+test "stale Unix socket is removed and can be rebound" {
+    const io = std.testing.io;
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    var directory_path_buffer: [std.fs.max_path_bytes]u8 = undefined;
+    const directory_path_len = try tmp.dir.realPath(io, &directory_path_buffer);
+    const socket_path = try std.fmt.allocPrint(
+        std.testing.allocator,
+        "{s}/rvw.sock",
+        .{directory_path_buffer[0..directory_path_len]},
+    );
+    defer std.testing.allocator.free(socket_path);
+    const address = try std.Io.net.UnixAddress.init(socket_path);
+
+    var first = try address.listen(io, .{});
+    first.deinit(io);
+    try removeStaleSocket(io, .cwd(), socket_path);
+
+    var rebound = try address.listen(io, .{});
+    rebound.deinit(io);
+    try removeStaleSocket(io, .cwd(), socket_path);
+}
+
+test "stale socket cleanup preserves non-socket files" {
+    const io = std.testing.io;
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    var file = try tmp.dir.createFile(io, "rvw.sock", .{});
+    file.close(io);
+    try removeStaleSocket(io, tmp.dir, "rvw.sock");
+
+    const stat = try tmp.dir.statFile(io, "rvw.sock", .{});
+    try std.testing.expectEqual(std.Io.File.Kind.file, stat.kind);
 }
